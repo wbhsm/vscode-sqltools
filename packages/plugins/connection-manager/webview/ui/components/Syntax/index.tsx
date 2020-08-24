72a268c843741c21de98f27382e63bd038718184
Matheus <matheus.mtxr@gmail.com>
2020-08-24 01:15:39 -0300

    [react-hooks] react hooks for settings form

--- packages/plugins/connection-manager/webview/ui/components/Syntax/index.tsx
+++ packages/plugins/connection-manager/webview/ui/components/Syntax/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { clipboardInsert } from '../../lib/utils';
 import styles from './style.m.scss';
 interface SyntaxProps {
@@ -10,50 +10,57 @@ interface SyntaxProps {
   style?: React.CSSProperties;
 }
 
-interface SyntaxState {
-  copyMsg: string;
-}
-export default class Syntax extends React.Component<SyntaxProps, SyntaxState> {
-  private id = `syntax-${(Math.random() * 1000).toFixed(0)}`;
+const Syntax = ({
+  code,
+  language,
+  width,
+  strong,
+  allowCopy,
+  style,
+}: SyntaxProps) => {
+  const [copyMsg, setCopyMsg] = useState('Copy');
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      copyMsg: 'Copy',
-    };
-  }
-  copyCode = () => {
-    clipboardInsert(JSON.stringify(this.props.code, null, 2));
-    this.setState({ copyMsg: 'Copied!' }, () => {
-      setTimeout(() => {
-        this.setState({ copyMsg: 'Copy' });
-      }, 1000);
-    });
-  }
+  const copyCode = useCallback(() => {
+    clipboardInsert(JSON.stringify(code, null, 2));
+    setCopyMsg('Copied!');
+    setTimeout(() => setCopyMsg('Copy'), 1000);
+  }, [copyMsg]);
 
-  renderCode = (code) => {
-    if (this.props.language === 'json' && typeof code === 'object') {
-      return JSON.stringify(code, null, 2 )
+  return (
+    <main className={styles.syntaxContainer} style={{ width: width, ...style }}>
+      <section
+        className={`${strong ? styles.syntaxStrongBg : styles.syntax} ${
+          language ? 'syntax-' + language : ''
+        }`}
+        dangerouslySetInnerHTML={{
+          __html: stringifyCode(code, language),
+        }}
+      ></section>
+      {allowCopy && (
+        <button
+          className={styles.copyCodeButton}
+          type='button'
+          onClick={copyCode}
+        >
+          {copyMsg}
+        </button>
+      )}
+    </main>
+  );
+};
+
+export default Syntax;
+
+const stringifyCode = (code: object, language?: string) => {
+  if (language === 'json' && typeof code === 'object') {
+    return JSON.stringify(code, null, 2)
       .replace(/( *)(".+") *:/g, '$1<span class="key">$2</span>:')
       .replace(/: *(".+")/g, ': <span class="string">$1</span>')
       .replace(/: *([0-9]+(\.[0-9]+)?)/g, ': <span class="number">$1</span>')
       .replace(/: *(null|true|false)/g, ': <span class="bool">$1</span>');
-    }
-    if (typeof code === 'string') {
-      return code;
-    }
-    return JSON.stringify(code);
   }
-  public render() {
-    return (
-      <main className={styles.syntaxContainer} style={{ width: this.props.width, ...this.props.style }}>
-        <section
-          id={this.id}
-          className={`${this.props.strong ? styles.syntaxStrongBg : styles.syntax} ${this.props.language ? 'syntax-' + this.props.language : ''}`}
-          dangerouslySetInnerHTML={{ __html: this.renderCode(this.props.code) }}
-        ></section>
-        {this.props.allowCopy && <button className={styles.copyCodeButton} type='button' onClick={this.copyCode}>{this.state.copyMsg}</button>}
-      </main>
-    );
+  if (typeof code === 'string') {
+    return code;
   }
-}
+  return JSON.stringify(code);
+};
